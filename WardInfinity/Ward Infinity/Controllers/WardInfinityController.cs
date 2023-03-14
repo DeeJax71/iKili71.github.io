9524c03c725c03d76b1567757228c9788a507554
Kai <100342931+DeeJax71@users.noreply.github.com>
2023-03-14 13:05:11 +1300

    Initial Commit

---
+++ WardInfinity/Ward Infinity/Controllers/WardInfinityController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ward_Infinity.Controllers
+{
+    public class WardInfinityController : Controller
+    {
+        // GET: WardInfinity
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Game()
+        {
+            return View();
+        }
+        public ActionResult News()
+        {
+            return View();
+        }
+        public ActionResult Support()
+        {
+            return View();
+        }
+        public ActionResult PlayNow()
+        {
+            return View();
+        }   
+        public ActionResult BuyPack()
+        {
+            return View();
+        }
+        public ActionResult WardInfinitySupport() 
+        {
+            return View();
+        }
+    }
+}
---
+++ WardInfinity/Ward Infinity/Models/WardInfinitySupport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace Ward_Infinity.Models
+{
+    public class WardInfinitySupport
+    {
+        public string UserID { get; set; }
+        public string Username { get; set; }
+        public string CharacterName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Button SubmitTicket { get; set; }
+        public Button GoBack { get; set; }
+       
+
+    }
+}
